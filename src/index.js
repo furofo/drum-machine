624457cefffde784bd01d15c735c907540e3f40c
furofo <chriscellphonerepair@gmail.com>
2020-06-02 20:19:01 -0400

    first logic for react -redux implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import { render } from 'react-dom'
+import './style.css';
+import $ from "jquery";
+import marked from 'marked';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log('hello world');
