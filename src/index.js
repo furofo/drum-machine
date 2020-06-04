71c3257b51739c195d60563c2ed07ca79d09f1ca
furofo <chriscellphonerepair@gmail.com>
2020-06-04 01:35:23 -0400

    used combine reducers method for state this time

--- src/index.js
+++ src/index.js
@@ -3,23 +3,72 @@ import ReactDom from 'react-dom';
 import { render } from 'react-dom'
 import './style.css';
 import $ from "jquery";
-
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
+import { combineReducers } from 'redux'
+const ACTION = 'ACTION';
+const ADDVOLUME = 'ADDVOLUME';
+const SUBTRACTVOLUME = 'SUBTRACTVOLUME';
 
-const DefaultState = {
-    power: true,
-    volume: 0,
-    bank: 'heaterKit',
-    heaterkit: {q: ['Heater1', '<audio src = "#"> </audio>']}
 
+
+const powerReducer = (state = {power: true}, action) => {
+    switch(action.type) {
+        case ACTION:
+            return {
+                power: false
+            }
+        default: 
+            return state
 }
-const reducer = function(state = DefaultState ) {
-    return state
+}
+const volumeReducer = (state = {volume: 0}, action) => {
+    switch(action.type) {
+        case ADDVOLUME: 
+            return {
+                volume: state.volume + action.addValue
+            }
+        
+
+        case SUBTRACTVOLUME:
+            return {
+                volume: state.volume - action.subtractValue
+            }
+        default:
+            return state
+        
+    }
 }
 
-let store = createStore(reducer);
+const kitReducer = (state = {kit: 'heaterKit'}, action) => {
+    switch(action.type) {
+        case 'pianoKit':
+            return {
+                kit: 'pianoKit'
+            }
+        case 'heaterKit':
+            return {
+                kit: 'heaterKit'
+            }
+        default:
+            return state
+    }
+}
 
+
+
+const rootReducer = combineReducers({
+    power: powerReducer,
+    volume: volumeReducer,
+    kit: kitReducer,
+})
+
+let store = createStore(rootReducer);
+
+console.log(store.getState());
+store.dispatch({type: ACTION});
+console.log(store.getState());
+store.dispatch({type: 'pianoKit'});
 console.log(store.getState());
